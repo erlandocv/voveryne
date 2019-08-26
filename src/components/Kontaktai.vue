ed03a87f749786a1211139cba7c0b0df09069f98
Erlandas <erlandasz@mail.com>
2019-08-26 20:18:41 +0300

    Responsiveness fixes

--- src/components/Kontaktai.vue
+++ src/components/Kontaktai.vue
@@ -5,7 +5,7 @@
         <v-img mx-auto :src="images.image1" max-height="760" contain>
           <v-container class="fill-height" fluid>
             <v-row align="center" justify="center">
-              <v-col cols="12" sm="8" md="4">
+              <v-col cols="12" sm="8" md="3">
                 <v-card color="rgb(170, 185, 186, 0.9)" class="elevation-12">
                   <v-card-title class="justify-center deep-purple--text text--darken-4">SUSISIEKITE</v-card-title>
                   <v-card-title
